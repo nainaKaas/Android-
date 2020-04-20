8d812b949b0ce1248614812e085cb19cb3f5b908
Naina Kashyap <nainakashyap888@gmail.com>
2020-04-21 00:57:35 +0530

    drawing app added

---
+++ Drawingapp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.drawingapp
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint: ImageButton? =
+        null // A variable for current color is picked from color pallet.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat()) // Setting the default brush size to drawing view.
+
+        /**
+         * This is to select the default Image button which is
+         * active and color is already defined in the drawing view class.
+         * As the default color is black so in our color pallet it is on 2 position.
+         * But the array list start position is 0 so the black color is at position 1.
+         */
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pallet_pressed
+            )
+        )
+
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    /**
+     * Method is used to launch the dialog to select different brush sizes.
+     */
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size :")
+        val smallBtn = brushDialog.ib_small_brush
+        smallBtn.setOnClickListener(View.OnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        })
+        val mediumBtn = brushDialog.ib_medium_brush
+        mediumBtn.setOnClickListener(View.OnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        })
+
+        val largeBtn = brushDialog.ib_large_brush
+        largeBtn.setOnClickListener(View.OnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        })
+        brushDialog.show()
+    }
+
+    // TODO(Step 2 - A function for color selection.)
+    /**
+     * Method is called when color is clicked from pallet_normal.
+     *
+     * @param view ImageButton on which click took place.
+     */
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
+            // Update the color
+            val imageButton = view as ImageButton
+            // Here the tag is used for swaping the current color with previous color.
+            // The tag stores the selected view
+            val colorTag = imageButton.tag.toString()
+            // The color is set as per the selected tag here.
+            drawing_view.setColor(colorTag)
+            // Swap the backgrounds for last active and currently active image button.
+            imageButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_pressed))
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.pallet_normal
+                )
+            )
+
+            //Current view is updated with selected view in the form of ImageButton.
+            mImageButtonCurrentPaint = view
+        }
+    }
+}
